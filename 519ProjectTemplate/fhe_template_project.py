b95008fd1ecfdc9a1b80b3c7959cc3c953c1e7cc
Can Özsoy <can.ozsoy@beefull.com>
2022-05-19 17:23:34 +0000

    auto indent

--- 519ProjectTemplate/fhe_template_project.py
+++ 519ProjectTemplate/fhe_template_project.py
@@ -8,9 +8,11 @@ from random import random
 
 # Using networkx, generate a random graph
 # You can change the way you generate the graph
+
+
 def generateGraph(n, k, p):
     #ws = nx.cycle_graph(n)
-    ws = nx.watts_strogatz_graph(n,k,p)
+    ws = nx.watts_strogatz_graph(n, k, p)
     return ws
 
 # If there is an edge between two vertices its weight is 1 otherwise it is zero
@@ -18,53 +20,64 @@ def generateGraph(n, k, p):
 # Two dimensional adjacency matrix is represented as a vector
 # Assume there are n vertices
 # (i,j)th element of the adjacency matrix corresponds to (i*n + j)th element in the vector representations
-def serializeGraphZeroOne(GG,vec_size):
+
+
+def serializeGraphZeroOne(GG, vec_size):
     n = GG.size()
     graphdict = {}
     g = []
     for row in range(n):
         for column in range(n):
-            if GG.has_edge(row, column) or row==column: # I assumed the vertices are connected to themselves
+            # I assumed the vertices are connected to themselves
+            if GG.has_edge(row, column) or row == column:
                 weight = 1
             else:
-                weight = 0 
-            g.append( weight  )  
+                weight = 0
+            g.append(weight)
             key = str(row)+'-'+str(column)
-            graphdict[key] = [weight] # EVA requires str:listoffloat
+            graphdict[key] = [weight]  # EVA requires str:listoffloat
     # EVA vector size has to be large, if the vector representation of the graph is smaller, fill the eva vector with zeros
-    for i in range(vec_size - n*n): 
+    for i in range(vec_size - n*n):
         g.append(0.0)
     return g, graphdict
 
 # To display the generated graph
-def printGraph(graph,n):
+
+
+def printGraph(graph, n):
     for row in range(n):
         for column in range(n):
-            print("{:.5f}".format(graph[row*n+column]), end = '\t')
-        print() 
+            print("{:.5f}".format(graph[row*n+column]), end='\t')
+        print()
 
 # Eva requires special input, this function prepares the eva input
 # Eva will then encrypt them
+
+
 def prepareInput(n, m):
     input = {}
-    GG = generateGraph(n,3,0.5)
-    graph, graphdict = serializeGraphZeroOne(GG,m)
+    GG = generateGraph(n, 3, 0.5)
+    graph, graphdict = serializeGraphZeroOne(GG, m)
     input['Graph'] = graph
     return input
 
 # This is the dummy analytic service
 # You will implement this service based on your selected algorithm
-# you can other parameters using global variables !!! do not change the signature of this function 
+# you can other parameters using global variables !!! do not change the signature of this function
+
+
 def graphanalticprogram(graph):
-    reval = graph<<1 ## Check what kind of operators are there in EVA, this is left shift
+    reval = graph << 1  # Check what kind of operators are there in EVA, this is left shift
     # Note that you cannot compute everything using EVA/CKKS
     # For instance, comparison is not possible
     # You can add, subtract, multiply, negate, shift right/left
     # You will have to implement an interface with the trusted entity for comparison (send back the encrypted values, push the trusted entity to compare and get the comparison output)
     return reval
-    
-# Do not change this 
+
+# Do not change this
 # the parameter n can be passed in the call from simulate function
+
+
 class EvaProgramDriver(EvaProgram):
     def __init__(self, name, vec_size=4096, n=4):
         self.n = n
@@ -80,6 +93,8 @@ class EvaProgramDriver(EvaProgram):
 # You can modify the input parameters
 # n is the number of nodes in your graph
 # If you require additional parameters, add them
+
+
 def simulate(n):
     m = 4096*4
     print("Will start simulation for ", n)
@@ -90,12 +105,12 @@ def simulate(n):
     config['balance_reductions'] = 'true'
     inputs = prepareInput(n, m)
 
-    graphanaltic = EvaProgramDriver("graphanaltic", vec_size=m,n=n)
+    graphanaltic = EvaProgramDriver("graphanaltic", vec_size=m, n=n)
     with graphanaltic:
         graph = Input('Graph')
         reval = graphanalticprogram(graph)
         Output('ReturnedValue', reval)
-    
+
     prog = graphanaltic
     prog.set_output_ranges(30)
     prog.set_input_scales(30)
@@ -103,53 +118,59 @@ def simulate(n):
     start = timeit.default_timer()
     compiler = CKKSCompiler(config=config)
     compiled_multfunc, params, signature = compiler.compile(prog)
-    compiletime = (timeit.default_timer() - start) * 1000.0 #ms
+    compiletime = (timeit.default_timer() - start) * 1000.0  # ms
 
     start = timeit.default_timer()
     public_ctx, secret_ctx = generate_keys(params)
-    keygenerationtime = (timeit.default_timer() - start) * 1000.0 #ms
-    
+    keygenerationtime = (timeit.default_timer() - start) * 1000.0  # ms
+
     start = timeit.default_timer()
     encInputs = public_ctx.encrypt(inputs, signature)
-    encryptiontime = (timeit.default_timer() - start) * 1000.0 #ms
+    encryptiontime = (timeit.default_timer() - start) * 1000.0  # ms
 
     start = timeit.default_timer()
     encOutputs = public_ctx.execute(compiled_multfunc, encInputs)
-    executiontime = (timeit.default_timer() - start) * 1000.0 #ms
+    executiontime = (timeit.default_timer() - start) * 1000.0  # ms
 
     start = timeit.default_timer()
     outputs = secret_ctx.decrypt(encOutputs, signature)
-    decryptiontime = (timeit.default_timer() - start) * 1000.0 #ms
+    decryptiontime = (timeit.default_timer() - start) * 1000.0  # ms
 
     start = timeit.default_timer()
     reference = evaluate(compiled_multfunc, inputs)
-    referenceexecutiontime = (timeit.default_timer() - start) * 1000.0 #ms
-    
+    referenceexecutiontime = (timeit.default_timer() - start) * 1000.0  # ms
+
     # Change this if you want to output something or comment out the two lines below
     for key in outputs:
         print(key, float(outputs[key][0]), float(reference[key][0]))
 
-    mse = valuation_mse(outputs, reference) # since CKKS does approximate computations, this is an important measure that depicts the amount of error
+    # since CKKS does approximate computations, this is an important measure that depicts the amount of error
+    mse = valuation_mse(outputs, reference)
 
     return compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse
 
 
 if __name__ == "__main__":
-    simcnt = 100 #The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
+    simcnt = 100  # The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
     # For benchmarking you must set it to a large number, e.g., 100
-    #Note that file is opened in append mode, previous results will be kept in the file
-    resultfile = open("results.csv", "a")  # Measurement results are collated in this file for you to plot later on
-    resultfile.write("NodeCount,PathLength,SimCnt,CompileTime,KeyGenerationTime,EncryptionTime,ExecutionTime,DecryptionTime,ReferenceExecutionTime,Mse\n")
+    # Note that file is opened in append mode, previous results will be kept in the file
+    # Measurement results are collated in this file for you to plot later on
+    resultfile = open("results.csv", "a")
+    resultfile.write(
+        "NodeCount,PathLength,SimCnt,CompileTime,KeyGenerationTime,EncryptionTime,ExecutionTime,DecryptionTime,ReferenceExecutionTime,Mse\n")
     resultfile.close()
-    
+
     print("Simulation campaing started:")
-    for nc in range(36,64,4): # Node counts for experimenting various graph sizes
+    for nc in range(36, 64, 4):  # Node counts for experimenting various graph sizes
         n = nc
-        resultfile = open("results.csv", "a") 
+        resultfile = open("results.csv", "a")
         for i in range(simcnt):
-            #Call the simulator
-            compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse = simulate(n)
-            res = str(n) + "," + str(1) + "," + str(i) + "," + str(compiletime) + "," + str(keygenerationtime) + "," +  str(encryptiontime) + "," +  str(executiontime) + "," +  str(decryptiontime) + "," +  str(referenceexecutiontime) + "," +  str(mse) + "\n"
+            # Call the simulator
+            compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse = simulate(
+                n)
+            res = str(n) + "," + str(1) + "," + str(i) + "," + str(compiletime) + "," + str(keygenerationtime) + "," + str(encryptiontime) + \
+                "," + str(executiontime) + "," + str(decryptiontime) + \
+                "," + str(referenceexecutiontime) + "," + str(mse) + "\n"
             print(res)
             resultfile.write(res)
         resultfile.close()
